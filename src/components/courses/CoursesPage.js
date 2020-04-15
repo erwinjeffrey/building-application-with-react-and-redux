dbe6be5cfad4d8315cf6a7c38b8923f9a53572e2
System Administrator <root@Maxs-MacBook-Pro.local>
2020-04-15 15:32:05 -0400

    building app with react-redux

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/CourseActions';
+import PropTypes from 'prop-types';
+
+class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ''
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createCourse(this.state.course);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Course</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
