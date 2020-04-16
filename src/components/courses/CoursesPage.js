24b6d8905bf619866df90a410a3cca67aad383a5
System Administrator <root@Maxs-MacBook-Pro.local>
2020-04-15 20:04:31 -0400

    adding authors api with thunk

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/CourseActions';
+import * as authorActions from '../../redux/actions/AuthorActions';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import CourseList from './CourseList';
 
 class CoursesPage extends Component {
   componentDidMount() {
-    this.props.actions.loadCourses().catch(error => {
-      alert(`Loading courses failed ${error}`);
-    });
+    // const { courses, authors, actions} = this.props;
+
+    if (this.props.courses.length === 0) {
+      this.props.actions.loadCourses().catch(error => {
+        alert(`Loading courses failed ${error}`);
+      });
+    }
+
+    if (this.props.authors.length === 0) {
+      this.props.actions.loadAuthors().catch(error => {
+        alert(`Loading authors failed ${error}`);
+      });
+    }
   }
   render() {
     return (
@@ -23,18 +34,31 @@ class CoursesPage extends Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state) {
   return {
-    courses: state.courses
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    authors: state.authors
   };
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    actions: bindActionCreators(courseActions, dispatch)
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
   };
 };
 export default connect(
--- src/redux/reducers/CourseReducer.js
+++ src/redux/reducers/CourseReducer.js
@@ -1,4 +1,5 @@
 import * as types from '../actions/ActionTypes';
+
 export default function courseReducer(state = [], action) {
   switch (action.type) {
     case types.CREATE_COURSE:
