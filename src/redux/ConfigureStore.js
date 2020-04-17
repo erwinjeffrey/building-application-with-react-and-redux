e48137b10aa5f2f70f8307a30fae4442efde5499
System Administrator <root@Maxs-MacBook-Pro.local>
2020-04-17 16:10:12 -0400

    adding production-builds

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,14 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers/index';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add suppport for Redux dev tools
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+// Use CommonJs require below so we can dynamically import during build-time
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./ConfigureStoreStore.prod');
+} else {
+  module.exports = require('./ConfigureStore-dev');
 }
